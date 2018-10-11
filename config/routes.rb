06e9e39bd8788f3c55420f492e5c94cf39dddce4
USUARIO <USUARIO@EMAIL.com>
2018-10-11 14:48:43 -0300

    modelos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'roles/index'
+  get 'roles/new'
+  post 'roles/create'
+  get 'roles/:id/edit', to: 'roles#edit', as: 'roles_edit'
+  patch 'roles/:id/update',  to: 'roles#update', as: 'roles_update'
+  delete 'roles/:id/destroy',to: 'roles#destroy', as: 'roles_delete'
+  
+
+  devise_for :users
+  get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
